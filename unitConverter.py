84fa8e495d7a789a37f6294df747dafb77a456c2
Brian Lobo <brianlobo.code@gmail.com>
2018-09-22 20:24:49 -0400

    Refracting code

--- unitConverter.py
+++ unitConverter.py
@@ -1,64 +1,5 @@
 import re
-import conversions
-
-def str_works(currentUnit, desiredUnit):
-	# Checks to see if micro is in either str
-	cUnit = currentUnit.find('micro')
-	dUnit = desiredUnit.find('micro')
-
-	# Checks to see if cUnit is micro or in the conversionTable
-	if cUnit == -1:
-		cUnit = currentUnit.strip()[-1] in conversions.metricConversions
-	else:
-		cUnit = True
-
-	# Checks to see if dUnit is micro or in the conversionTable
-	if dUnit == -1:
-		dUnit = desiredUnit.strip() in conversions.metricConversions
-	else:
-		dUnit = True
-
-	# If both are True, return true. Else return False!
-	if cUnit and dUnit:
-		return True
-	else:
-		return False
-
-# Returns the number from the current unit
-def grabNbr(str):
-	# regex that finds any int or float in the str and returns a list
-	nbr = (re.findall(r'\d+(?:\.\d+)?', str))
-	return float(nbr[0])
-
-def stripStr(currentUnit, desiredUnit):
-	nbr = grabNbr(currentUnit)
-	if currentUnit.find('micro') == -1:
-		currentUnit = currentUnit.strip()[-1]	# Removes whitespace around str and grabs unit
-	else:
-		currentUnit = 'micro'
-	if desiredUnit.find('micro') == -1:
-		desiredUnit = desiredUnit.strip()		# Same thing but str should only be a character
-	else:
-		desiredUnit = 'micro'
-
-	return nbr, currentUnit, desiredUnit
-
-def convert(currentUnit, desiredUnit):
-	nbr, currentUnit, desiredUnit = stripStr(currentUnit, desiredUnit)
-
-	conversion1 = conversions.metricConversions[currentUnit]
-	conversion2 = conversions.metricConversions[desiredUnit]
-
-	# If current unit isnt base, will multiply nbr to get it in base
-	if currentUnit != 'b':
-		nbr *= conversion1
-		if desiredUnit == 'b':
-			print('\n===================\n')
-			print(round(nbr, 4), desiredUnit)
-			return
-	nbr /= conversion2
-	print('\n===================\n')
-	print (round(nbr, 4), desiredUnit)
+import convert
 
 def print_abbreve_table():
 	print ('\nAbbreviations for units:\n|\t(exactly as written)\t|')
@@ -97,8 +38,8 @@ def main():
 
 	# Checks to see if input is valid
 	# If valid, will preform conversion
-	if str_works(currentUnit, desiredUnit) == True:
-		convert(currentUnit, desiredUnit)
+	if convert.str_works(currentUnit, desiredUnit) == True:
+		convert.convert(currentUnit, desiredUnit)
 	else:
 		user_types_error()
 
